9c8195f3f52adb9e5cb8f3e0083546f6e86759c5
James Goodhand <james.goodhand@synapse256.co.uk>
2023-08-30 15:54:00 +0100

    fix 🐛 (appsync_resolver): fix bug preventing appsync_js resolvers for UNIT kind resolvers

--- main.tf
+++ main.tf
@@ -201,7 +201,7 @@ resource "aws_appsync_resolver" "this" {
   }
 
   # code is required when runtime is APPSYNC_JS
-  code = try(each.value.kind == "PIPELINE" && each.value.runtime.name == "APPSYNC_JS", false) ? each.value.code : null
+  code = try(each.value.runtime.name == "APPSYNC_JS", false) ? each.value.code : null
 
   request_template  = lookup(each.value, "request_template", tobool(lookup(each.value, "direct_lambda", false)) ? var.direct_lambda_request_template : try(each.value.kind == "PIPELINE" && each.value.runtime.name == "APPSYNC_JS", false) ? null : "{}")
   response_template = lookup(each.value, "response_template", tobool(lookup(each.value, "direct_lambda", false)) ? var.direct_lambda_response_template : try(each.value.kind == "PIPELINE" && each.value.runtime.name == "APPSYNC_JS", false) ? null : "{}")
